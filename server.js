3cab23796a4361f1afce623542c120cf1ad86a9b
Sutan Nasution <sutan.gnst@gmail.com>
2020-04-05 00:29:52 +0700

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 
 const port = process.env.PORT || 3000
 const server = express()
 
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
